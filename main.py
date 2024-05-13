8e248cf2a9a6ef2152511e5dc93e6f4572f6ecf0
Eddiez26 <eddiez15@nycstudents.net>
2024-05-13 08:42:20 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import time
 from background import Background
-from apple import Apple
+from fruit import Pineapple
 
 
 # set up variables for the display
@@ -18,11 +18,11 @@ b = 100
 
 
 
-
+P=Pineapple(20,30)
 B = Background(40, 40)
 
 
-A=Apple(20,20)
+
 
 # The loop will carry on until the user exits the game (e.g. clicks the close button).
 run = True
@@ -30,12 +30,45 @@ run = True
 # -------- Main Program Loop -----------
 while run:
 
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                change_to = 'UP'
+            if event.key == pygame.K_DOWN:
+                change_to = 'DOWN'
+            if event.key == pygame.K_LEFT:
+                change_to = 'LEFT'
+            if event.key == pygame.K_RIGHT:
+                change_to = 'RIGHT'
+
+            if change_to == 'UP' and direction != 'DOWN':
+                direction = 'UP'
+            if change_to == 'DOWN' and direction != 'UP':
+                direction = 'DOWN'
+            if change_to == 'LEFT' and direction != 'RIGHT':
+                direction = 'LEFT'
+            if change_to == 'RIGHT' and direction != 'LEFT':
+                direction = 'RIGHT'
+
+            if direction == 'UP':
+                snake_position[1] -= 10
+            if direction == 'DOWN':
+                snake_position[1] += 10
+            if direction == 'LEFT':
+                snake_position[0] -= 10
+            if direction == 'RIGHT':
+                snake_position[0] += 10
+
+
+
+
 
 
     ## FILL SCREEN, and BLIT here ##
     screen.fill((r, g, b))
     screen.blit(B.image, B.rect)
     screen.blit(A.image, A.rect)
+    screen.blit(P.image, P.rect)
     pygame.display.update()
     ## END OF WHILE LOOP
 
@@ -43,6 +76,4 @@ while run:
 
 # Once we have exited the main program loop we can stop the game engine:
 pygame.quit()
-
-
 
